b86c9f5932e901c14c0ef0bfbc12da5ae4e93a90
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-04-10 20:50:04 +0900

    GU スタッフスタイリング検索をおこなうためのルーティングを作成する。

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
+    Route::get('/', 'RootsController@index') -> name('root');
+    Route::resource('/gu_staff', 'GU_StaffsController') -> only(['store']);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// ↓ デフォルトの Welcome ページへのルーティング ↓
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+?>
