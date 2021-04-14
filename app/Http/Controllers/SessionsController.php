53495c1824affc885ef092c7f3c39b9aaab2e586
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-04-14 16:11:19 +0900

    ログインフォームをサイドバー部分に移動する。

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
     public function create()
     {
-        return view('sessions/create');
+        $all_hash_tags = array(
+            // ↓ レディース，メンズ 共通のハッシュタグ ↓
+            '#春カラー', '#きれいめコーデ', '#楽チンコーデ', '#大人コーデ', '#春休みコーデ', '#きれいめカジュアル', '#お花見コーデ', '#新生活', '#リモートワーク', '#夏コーデ', '#カジュアルコーデ',
+
+            // ↓ レディースのハッシュタグ ↓
+            '#大人カジュアル',   '#フェミニントップス', '#春夏コーデ',  '#ワンピース', '#デニム',
+
+            // ↓ メンズのハッシュタグ ↓
+            '#レイヤード', '#オーバーサイズ', '#オープンカラーシャツ', '#大人カジュアル', '#オトナカジュアル'
+        );
+
+        $hash_tags = array();
+        foreach(array_rand ($all_hash_tags , 5) as $tags_key) {
+            array_push ( $hash_tags, $all_hash_tags[$tags_key] );
+        }
+
+        return view('sessions/create', ['all_hash_tags' => $all_hash_tags, 'hash_tags' => $hash_tags]);
     }
 
     public function store(Request $request)
@@ -22,7 +37,7 @@ class SessionsController extends Controller
         }
     }
 
-    public function destroy(User $user)
+    public function destroy()
     {
         Auth::logout();
         return redirect() -> route('root');
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -2,103 +2,97 @@
 @section('title', '既存ユーザー ログインページ | Multi-0-Design')
 
 @section('content')
-<section class='Sign-Up container-lg'>
-    <form action="{{ route('user_session') }}" method='POST' enctype='application/x-www-form-urlencoded' accept-charset='UTF-8'>
-        @csrf
-        <div class='form-group'>
-            <p class=''>
-                <span class='badge badge-danger'>必須</span>
-                <label for='email'>メールアドレス</label>：
-            </p>
-            <input type='email' name='email' class='form-control' id='email' inputmode='email' autocomplete='email' required autofocus />
-        </div>
+    <div class='container-lg'>
+        <div class='row'>
+            <section class='Side_Menu col-3'>
+                <h2 class='Title_Menu' data-en='Menu'>
+                    <span><i class='fas fa-bars'></i>メニュー</span>
+                </h2>
 
-        <div class='form-group'>
-            <p class=''>
-                <span class='badge badge-danger'>必須</span>
-                <label for='password'>パスワード</label>：
-            </p>
-            <input type='password' name='password' class='form-control' id='password' inputmode='text' autocomplete='off' required />
-        </div>
+                <a href="{{ route('root') }}" class='Side_Menu__Home btn btn-outline-dark'>
+                    <i class='fas fa-home'></i>ホーム
+                </a>
 
-        <div class='form-group'>
-            <input type='submit' value='ログインする' class='form-control btn btn-primary' />
-        </div>
-    </form>
-</section>
+                @guest
+                    <a href="{{ route('new_user_session') }}" class='Side_Menu__SignIn btn btn-outline-dark'>
+                        <i class='fas fa-sign-in-alt'></i>ログイン
+                    </a>
 
+                    <a href="{{ route('new_user_registration') }}" class='Side_Menu__SignUp btn btn-outline-dark'>
+                        <i class='fas fa-user-plus'></i>新規登録
+                    </a>
+                @endguest
 
+                <h2 class='Title_Login' data-en='Login'>
+                    <span><i class='fas fa-sign-in-alt'></i>ログイン</span>
+                </h2>
 
+                <form action="{{ route('user_session') }}" name='SignIn_Form' method='POST' enctype='application/x-www-form-urlencoded' accept-charset='UTF-8'>
+                    @csrf
+                    <div class='Side_Menu-Login form-group'>
+                        <label for='email'><i class='fas fa-at'></i>メールアドレス</label>：
+                        <input type='email' name='email' class='Side_Menu-Login--Input form-control' id='email' inputmode='email' autocomplete='email' required />
+                    </div>
 
+                    <div class='Side_Menu-Login form-group'>
+                        <label for='password'><i class='fas fa-key'></i>パスワード</label>：
+                        <input type='password' name='password' class='Side_Menu-Login--Input form-control' id='password' inputmode='text' autocomplete='off' required />
+                    </div>
 
+                    <div class='Side_Menu-Login form-group'>
+                        <a href="javascript:document.SignIn_Form.submit()" class='Side_Menu-Login--Submit btn btn-outline-primary'>
+                            ログイン
+                        </a>
+                    </div>
+                </form>
+            </section>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <section class='GU_Staff col'>
+                <div class='GU_Staff__Title'>
+                    <h2>
+                        <picture>
+                            <source type='image/png' srcset="{{ asset('/images/GU.webp') }}" />
+                            <img src="{{ asset('/images/GU.png') }}" alt='' />
+                        </picture>
 
-                <div class="card-body">
-                    <form method="POST" action='#'>
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        <span class='GU_Staff__Title--Text'>
+                            GU<span class='GU_Staff__Title--Small'>（ジーユー）</span>STAFF STYLING 検索
+                        </span>
+                    </h2>
+                    <p class='GU_Staff__Title--Mute text-muted'>ジーユーのおしゃリスタ（コーディネートアドバイザー）のスタイリングコレクション</p>
                 </div>
-            </div>
+
+                <form action={{ route('gu_staff.store') }} class='GU_Staff__Form' id='GU-Staff_Form' target='_blank' method='POST'>
+                    @csrf
+                    <div class='GU_Staff__Form__Gender'>
+                        <p class='GU_Staff__Form__Gender--Title badge badge-secondary'>性別</p>
+                        <div class='GU_Staff__Form__Gender__Ladies form-check'>
+                            <input type='radio' name='gender' value='women' class='form-check-input' id='GU-Staff_Ladies' checked='checked' />
+                            <label class='form-check-label' for='GU-Staff_Ladies'>レディース</label>
+                        </div>
+                        <div class='form-check'>
+                            <input type='radio' name='gender' class='form-check-input' id='GU-Staff_Mens' value='men' />
+                            <label class='form-check-label' for='GU-Staff_Mens'>メンズ</label>
+                        </div>
+                    </div>
+
+                    <div class='GU_Staff__Form__Hash-Tag'>
+                        @foreach($hash_tags as $hash_tag)
+                            <input type='submit' name='tag' value="{{ $hash_tag }}" class='GU_Staff__Form__Hash-Tag--Link btn btn-outline-dark' />
+                        @endforeach
+
+                        <div class='GU_Staff__Form__Hash-Tag__Select'>
+                            <label for='GU-Staff_Form' class='text-muted'> その他のハッシュタグ：</label>
+                            <select id='GU-Staff_Form' name='tag'>
+                                @foreach($all_hash_tags as $hash_tag)
+                                    <option value="{{ $hash_tag }}"> {{ $hash_tag }} </option>
+                                @endforeach
+                            </select>
+                            <input type='submit' value='送信' class='GU_Staff__Form__Hash-Tag__Select--Submit btn btn-outline-dark' />
+                        </div>
+                    </div>
+                </form>
+            </section>
         </div>
     </div>
-</div>
 @endsection
