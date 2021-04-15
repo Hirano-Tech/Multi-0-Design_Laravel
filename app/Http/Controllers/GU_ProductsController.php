d16fe97f7659457b85ad96238a92b75cd712c25e
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-04-15 11:03:36 +0900

    GU の商品情報を保存するためのコントローラを作成する。

---
+++ app/Http/Controllers/GU_ProductsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GU_ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new GU_Product;
+        $product -> product_id = $request -> input('product_id');
+
+        $client = new Client();
+        $crawler = $client -> request('GET', "https://www.gu-global.com/jp/ja/products/E{$request -> input('product_id')}-000/00");
+        $product -> name = rtrim($crawler -> filter('title') -> text(), ' | GU(ジーユー)公式通販オンラインストア');
+        $product -> user_id = Auth::id();
+        $product -> save();
+
+        return redirect() -> route('root');    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
