253bb5f1c6cebc3fda5f27079eb7492a4b62f7ee
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2021-04-17 10:26:39 +0900

    ヘッダー部分を編集する。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
 
         <title>@yield('title')</title>
         <link rel='stylesheet' href="{{ asset('/css/app.css') }}" />
-        <link rel='stylesheet' href="{{ asset('/css/header.css') }}" />
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
 
@@ -17,44 +16,42 @@
                 <h1><a href={{ route('root') }}>Multi-0-Design</a></h1>
 
                 <div class='Header__Menu'>
-                    <a href="{{ route('root') }}" class='Header__Menu--Top btn btn-outline-dark'>
-                        <i class='fas fa-home'></i><br />ホーム
-                    </a>
-
                     @auth
                         <form action="{{ route('destroy_user_session') }}" name='SignOut_Form' method='POST'>
                             <input type='hidden' name='_method' value='DELETE' />
                             <input type='hidden' name='_token' value="{{ csrf_token() }}" />
-                            <a href="javascript:document.SignOut_Form.submit()" class='Header__Menu--SignOut btn btn-outline-dark'>
-                                <i class='fas fa-sign-out-alt'></i><br />ログアウト
+                            <a href='javascript:document.SignOut_Form.submit()' class='Header__Menu--SignOut btn btn-outline-danger'>
+                                <i class='fas fa-sign-out-alt'></i>ログアウト
                             </a>
                         </form>
                     @endauth
 
                     @guest
-                        <a href="{{ route('new_user_session') }}" class='Header__Menu--SignIn btn btn-outline-dark'>
-                            <i class='fas fa-sign-in-alt'></i><br />ログイン
-                        </a>
-                        <a href="{{ route('new_user_registration') }}" class='Header__Menu--SignUp btn btn-outline-dark'>
-                            <i class='fas fa-user-plus'></i><br />新規登録
-                        </a>
+                        @if (!str_contains(url() -> current(), 'users/sign_in'))
+                            <a href="{{ route('new_user_session') }}" class='Header__Menu--SignIn btn btn-outline-dark'>
+                                <i class='fas fa-sign-in-alt'></i>ログイン
+                            </a>
+                        @else
+                            <a href="{{ route('new_user_registration') }}" class='Header__Menu--SignIn btn btn-outline-dark'>
+                            <i class='fas fa-user-plus'></i>新規登録
+                            </a>
+                        @endif
                     @endguest
+                </div>
+
+                <div class='Mobile__Menu'>
+                    <button type='button' class='btn btn-light dropdown-toggle' id='dropdownMenuButton' data-toggle='dropdown'  >
+                        <i class='fas fa-bars'></i>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
                 </div>
             </div>
         </header>
 
         @yield('content')
-
-        <footer class='Footer container-fluid'>
-            <a href="{{ route('root') }}" class='Footer--Top btn btn-outline-dark'>
-                <i class='fas fa-home'></i><br />ホーム
-            </a>
-            <a href="{{ route('new_user_session') }}" class='Footer--SignIn btn btn-outline-dark'>
-                <i class="fas fa-sign-in-alt"></i><br />ログイン
-            </a>
-            <a href="{{ route('new_user_registration') }}" class='Footer--SignUp btn btn-outline-dark'>
-                <i class='fas fa-user-plus'></i><br />新規登録
-            </a>
-        </footer>
     </body>
 </html>
